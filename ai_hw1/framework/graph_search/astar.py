7d1391dc51b1e9e72addd43f61280c767c256940
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-11-14 20:44:27 +0200

    _calc_node_expanding_priority first imple

--- ai_hw1/framework/graph_search/astar.py
+++ ai_hw1/framework/graph_search/astar.py
@@ -50,7 +50,8 @@ class AStar(BestFirstSearch):
         Notice: You may use `search_node.g_cost`, `self.heuristic_weight`, and `self.heuristic_function`.
         """
 
-        raise NotImplementedError  # TODO: remove this line!
+        return ((1-self.heuristic_weight)*search_node.g_cost)+(self.heuristic_weight*self.heuristic_function(search_node))
+
 
     def _open_successor_node(self, problem: GraphProblem, successor_node: SearchNode):
         """
